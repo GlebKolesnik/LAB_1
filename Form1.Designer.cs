a610b5a2e8866cafbcfed2b522f9bfd17e2ceb0e
Hlib <gl.kolesnichenko@gmail.com>
2022-12-27 03:42:25 +0200

    1
    1.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LAB_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Table1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Table1
+            // 
+            this.Table1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table1.Location = new System.Drawing.Point(0, 0);
+            this.Table1.Name = "Table1";
+            this.Table1.RowHeadersWidth = 51;
+            this.Table1.RowTemplate.Height = 24;
+            this.Table1.Size = new System.Drawing.Size(935, 637);
+            this.Table1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 638);
+            this.Controls.Add(this.Table1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Table1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            doSom();
+        }
+        public void doSom()
+        {
+            DataGridViewTextBoxColumn column0 = new DataGridViewTextBoxColumn();
+            column0.Name = "Num";
+            column0.HeaderText = "№";
+
+            DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+            column1.Name = "A";
+            column1.HeaderText = "A1";
+
+            DataGridViewTextBoxColumn column2 = new DataGridViewTextBoxColumn();
+            column2.Name = "B";
+            column2.HeaderText = "B1";
+
+            DataGridViewTextBoxColumn column3 = new DataGridViewTextBoxColumn();
+            column3.Name = "C";
+            column3.HeaderText = "C1";
+
+            Table1.Columns.AddRange(column0,column1,column2,column3);
+
+            DataGridViewCell Num = new DataGridViewTextBoxCell();
+            DataGridViewCell A = new DataGridViewTextBoxCell();
+            DataGridViewCell B = new DataGridViewTextBoxCell();
+            DataGridViewCell C = new DataGridViewTextBoxCell();
+
+            Num.Value = "Here ";
+            A.Value = "will";
+            B.Value = "be";
+            C.Value = "realized";
+
+            DataGridViewRow row0 = new DataGridViewRow();
+            row0.Cells.AddRange(Num, A , B , C);
+            //Table1.Rows.Add(row0);
+
+            DataGridViewCell Num0 = new DataGridViewTextBoxCell();
+            DataGridViewCell A0 = new DataGridViewTextBoxCell();
+            DataGridViewCell B0 = new DataGridViewTextBoxCell();
+            DataGridViewCell C0 = new DataGridViewTextBoxCell();
+
+            Num.Value = "opportunity";
+            A.Value = "to input";
+            B.Value = "values wich";
+            C.Value = "you will input from buttons in program";
+
+            DataGridViewRow row1 = new DataGridViewRow();
+            row1.Cells.AddRange(Num0, A0, B0, C0);
+            Table1.Rows.AddRange(row0 ,row1);
+
+        }
+    }
+}
